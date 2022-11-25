201547d6552c2d7b50d168c6f8177d81d8ef852b
sheema <sheemashamilfarook@gmail.com>
2022-11-25 10:52:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') { 
+    return(`This Saturday, I want to ${activity}!`); 
+  } 
+saturdayFun('roller-skate'); 
+ 
+function mondayWork(activity = 'go to the office'){ 
+    return(`This Monday, I will ${activity}.`); 
+} 
+mondayWork('work from home.'); 
+ 
+function wrapAdjective(wrapper = "*") { 
+    function message(string = "special") { 
+      return `You are ${wrapper}${string}${wrapper}!`; 
+    }; 
+    return message; 
+  }; 
+wrapAdjective("||"); 
+result("a hard worker");
